6d2836de75c55ebec61c13afd391dce0c036740d
Eyad <eyad@yahoo.com>
2023-09-13 15:50:22 -0700

    task-11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+* Add _Adds two number and
+*@n1: The first number to be computed
+*@n2: The second number to be computed
+*Return integer value
+**/
+
+int add(int n1 , int n2 );
+
+{
+   
+   return (n1 + n2);
+}
