54e478fa6f917702d2d1262e7bfb1a0ea3514b5e
Eyad <eyad@yahoo.com>
2023-09-19 14:09:43 -0700

    task-4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,13 +6,13 @@
  * @s: the string
 **/
 
-void print_rev(char *s);
+void print_rev(char *s)
 {
 	int i;
 
 	for (i = strlen(s) - 1; i >= 0; i--)
 	{
-		_putchar(s[i]);
+	_putchar(s[i]);
 	}
 
 	_putchar('\n');
