fb069a08ae56a46394e9ef70a62af8457408655b
Eyad <eyad@yahoo.com>
2023-09-19 14:49:22 -0700

    task-6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,17 +5,17 @@
  * @str: string to print
 **/
 
-void puts2(char *str);
+void puts2(char *str)
 
 {
 	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+	if (i % 2 == 0)
+	{
+	_putchar(str[i]);
+	}
 	}
 
 	_putchar('\n');
