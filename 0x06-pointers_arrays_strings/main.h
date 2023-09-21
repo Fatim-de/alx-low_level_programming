3ed337770327c3941c4d9bd7f39f19d86b5536d6
Eyad <eyad@yahoo.com>
2023-09-21 13:34:56 -0700

    task-8

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -16,4 +16,12 @@ char *cap_string(char *);
 
 char *leet(char *);
 
+void print_buffer(char *b, int size);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_number(int n);
+
+char *rot13(char *);
+
 
