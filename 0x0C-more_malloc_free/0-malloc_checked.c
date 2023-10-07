a244630fb105ab07a51f16205c8dd25098bf7c40
Eyad <eyad@yahoo.com>
2023-10-07 13:01:25 -0700

    task-0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked- check the code
+ * @b: number of bytes should be allocated
+ * Return : a pointer to the allocated memory
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+	{
+	exit(98);
+	}
+	return (p);
+}
