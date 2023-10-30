35c76f8a42b9d7ae27aff0a5f3b69fc74a3ebf5c
Fatim-de <bakrey222bbm@yahoo.com>
2023-10-30 14:31:43 -0700

    rr

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+n = read_textfile(av[1], 114);
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
